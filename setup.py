ec50cd9eb271e91e534c838925177ded9442bb61
Luciano Rossi <lukio@gcoop.coop>
2020-06-10 10:37:34 -0300

    Update setup. Update tests

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# This file is part of the sale_subscription_payment_collect module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 
@@ -7,24 +6,30 @@ import io
 import os
 import re
 from configparser import ConfigParser
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
-def read(fname):
-    return io.open(
+def read(fname, slice=None):
+    content = io.open(
         os.path.join(os.path.dirname(__file__), fname),
         'r', encoding='utf-8').read()
+    if slice:
+        content = '\n'.join(content.splitlines()[slice])
+    return content
 
 
 def get_require_version(name):
-    require = '%s >= %s.%s, < %s.%s'
+    if minor_version % 2:
+        require = '%s >= %s.%s.dev0, < %s.%s'
+    else:
+        require = '%s >= %s.%s, < %s.%s'
     require %= (name, major_version, minor_version,
         major_version, minor_version + 1)
     return require
 
 
 config = ConfigParser()
-config.read_file(open('tryton.cfg'))
+config.read_file(open(os.path.join(os.path.dirname(__file__), 'tryton.cfg')))
 info = dict(config.items('tryton'))
 for key in ('depends', 'extras_depend', 'xml'):
     if key in info:
@@ -38,65 +43,101 @@ name = 'trytonar_sale_subscription_payment_collect'
 download_url = 'https://github.com/tryton-ar/sale_subscription_payment_collect/tree/%s.%s' % (
     major_version, minor_version)
 
-requires = []
+if minor_version % 2:
+    version = '%s.%s.dev0' % (major_version, minor_version)
+    download_url = (
+        'git+http://github.com/tryton-ar/%s#egg=%s-%s' % (
+            name.replace('trytonar','trytond'), name, version))
+local_version = []
+for build in ['CI_BUILD_NUMBER', 'CI_JOB_NUMBER', 'CI_JOB_ID']:
+    if os.environ.get(build):
+        local_version.append(os.environ[build])
+if local_version:
+    version += '+' + '.'.join(local_version)
+
+requires = ['requests']
 for dep in info.get('depends', []):
     if dep == 'payment_collect':
-        requires.append(get_require_version('trytonar_%s' % dep))
+        requires.append('trytonar_payment_collect @ git+https://github.com/tryton-ar/payment_collect.git@%s.%s#egg=trytonar_payment_collect-%s.%s' % (major_version, minor_version, major_version, minor_version))
     elif not re.match(r'(ir|res)(\W|$)', dep):
         requires.append(get_require_version('trytond_%s' % dep))
 requires.append(get_require_version('trytond'))
-
 tests_require = [get_require_version('proteus')]
-dependency_links =[
-    'https://github.com/tryton-ar/payment_collect/tarball/%s.%s#egg=trytonar_payment_collect-%s.%s' \
-        % (major_version, minor_version, major_version, minor_version),
-    ]
+dependency_links = []
 
 setup(name=name,
     version=version,
-    description='Tryton module that add paymode to sales subscriptions',
-    long_description=read('README'),
-    author='tryton-ar',
-    url='https://github.com/tryton-ar/sale_subscription_payment_collect',
+    description='',
+    long_description=read('README.rst'),
+    author='trytonar',
+    author_email='info@gcoop.coop',
+    url='http://github.com/tryton-ar/sale_subscription_payment_collect',
     download_url=download_url,
+    project_urls={
+        "Bug Tracker": 'https://bugs.tryton.org/',
+        "Documentation": 'https://docs.tryton.org/',
+        "Forum": 'https://www.tryton.org/forum',
+        "Source Code": 'https://github.com/gcoop-libre/trytond-sale_subscription_payment_collect',
+        },
+    keywords='tryton, sale, subscription, tiendas, portadeltiendas',
     package_dir={'trytond.modules.sale_subscription_payment_collect': '.'},
-    packages=[
-        'trytond.modules.sale_subscription_payment_collect',
-        'trytond.modules.sale_subscription_payment_collect.tests',
-        ],
+    packages=(
+        ['trytond.modules.sale_subscription_payment_collect']
+        + ['trytond.modules.sale_subscription_payment_collect.%s' % p
+            for p in find_packages()]
+        ),
     package_data={
         'trytond.modules.sale_subscription_payment_collect': (info.get('xml', [])
-            + ['tryton.cfg', 'view/*.xml', 'locale/*.po']),
+            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', '*.fodt',
+                'icons/*.svg', 'tests/*.rst']),
         },
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Plugins',
         'Framework :: Tryton',
         'Intended Audience :: Developers',
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'License :: OSI Approved :: '
+        'GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: Bulgarian',
+        'Natural Language :: Catalan',
+        'Natural Language :: Chinese (Simplified)',
+        'Natural Language :: Czech',
+        'Natural Language :: Dutch',
         'Natural Language :: English',
+        'Natural Language :: Finnish',
+        'Natural Language :: French',
+        'Natural Language :: German',
+        'Natural Language :: Hungarian',
+        'Natural Language :: Indonesian',
+        'Natural Language :: Italian',
+        'Natural Language :: Persian',
+        'Natural Language :: Polish',
+        'Natural Language :: Portuguese (Brazilian)',
+        'Natural Language :: Russian',
+        'Natural Language :: Slovenian',
         'Natural Language :: Spanish',
+        'Natural Language :: Turkish',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Office/Business',
-        'Topic :: Office/Business :: Financial :: Accounting',
         ],
     license='GPL-3',
-    python_requires='>=3.4',
+    python_requires='>=3.5',
     install_requires=requires,
     dependency_links=dependency_links,
     zip_safe=False,
     entry_points="""
     [trytond.modules]
     sale_subscription_payment_collect = trytond.modules.sale_subscription_payment_collect
-    """,
+    """,  # noqa: E501
     test_suite='tests',
     test_loader='trytond.test_loader:Loader',
     tests_require=tests_require,
